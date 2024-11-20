d4f502d57c2b1c67ceae4540cecbd4ab908ac907
buithaison17 <125809724+buithaison17@users.noreply.github.com>
2024-11-20 23:18:05 +0700

    Add files via upload

---
+++ baitap5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int chieuRong = 3;
+	int chieuDai = 4;
+	int dienTich = chieuDai * chieuRong;
+	int chuVi = (chieuDai + chieuRong) * 2;
+	printf("chieu dai cua hinh chu nhat la la: %d \n", chieuDai);
+	printf("chieu rong cua hinh chu nhat la la: %d \n", chieuRong);
+	printf("chu vi cua hinh chu nhat la: %d \n", chuVi);
+	printf("dien tich cua hinh chu nhat la: %d \n", dienTich);
+	
+}
